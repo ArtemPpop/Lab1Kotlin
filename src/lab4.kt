400aedc0174132335708f60e9afb43a762179fe7
ArtemPpop <144240156+ArtemPpop@users.noreply.github.com>
2024-12-09 15:23:59 +0200

    lab4.kt

--- src/lab4.kt
+++ src/lab4.kt
@@ -1,46 +1,38 @@
 //задание через list
 //fun main() {
-//    
-//    val numbers = listOf(1, 3, 5, 4, 2, 9, 7, 6, 8, 10)
-//
-//    
+//    println("Введите числа через пробел:")
+//    val numbers = readLine()!!.split(' ').map { it.toInt() }
 //    val count = numbers.indices.count { index ->
-//        index in 1 until numbers.size - 1 && 
-//                numbers[index] > numbers[index - 1] && 
-//                numbers[index] > numbers[index + 1]    
+//        index in 1 until numbers.size - 1 &&
+//                numbers[index] > numbers[index - 1] &&
+//                numbers[index] > numbers[index + 1]
 //    }
-//
-//    
-//    println("Количество элементов которые больше двух соседних: $count")
+//    println("Количество элементов, которые больше двух соседних: $count")
 //}
 
 //задание ерез set
-//fun main() {
-//   
-//    val (N, M) = readLine()!!.split(' ').map { it.toInt() }
-//
-//   
-//    val anjaCubes = mutableSetOf<Int>()
-//    repeat(N) {
-//        anjaCubes.add(readLine()!!.toInt())
-//    }
-//
-//   
-//    val boryaCubes = mutableSetOf<Int>()
-//    repeat(M) {
-//        boryaCubes.add(readLine()!!.toInt())
-//    }
-//
-//   
-//    val commonColors = anjaCubes.intersect(boryaCubes).sorted()
-//    val onlyAnjaColors = anjaCubes.subtract(boryaCubes).sorted()
-//    val onlyBoryaColors = boryaCubes.subtract(anjaCubes).sorted()
-//
-//    
-//    println("${commonColors.size} ${commonColors.joinToString(" ")}")
-//    println("${onlyAnjaColors.size} ${onlyAnjaColors.joinToString(" ")}")
-//    println("${onlyBoryaColors.size} ${onlyBoryaColors.joinToString(" ")}")
-//}
+fun main() {
+
+    println("Введите количество кубиков у Ани и Бориса (N M):")
+    val (N, M) = readLine()!!.split(' ').map { it.toInt() }
+    val anjaCubes = mutableSetOf<String>()
+    println("Введите $N цвета кубиков Ани:")
+    repeat(N) {
+        anjaCubes.add(readLine()!!)
+    }
+    val boryaCubes = mutableSetOf<String>()
+    println("Введите $M цвета кубиков Бориса:")
+    repeat(M) {
+        boryaCubes.add(readLine()!!)
+    }
+    val commonColors = anjaCubes.intersect(boryaCubes).sorted()
+    val onlyAnjaColors = anjaCubes.subtract(boryaCubes).sorted()
+    val onlyBoryaColors = boryaCubes.subtract(anjaCubes).sorted()
+
+    println("${commonColors.size} ${commonColors.joinToString(" ")}")
+    println("${onlyAnjaColors.size} ${onlyAnjaColors.joinToString(" ")}")
+    println("${onlyBoryaColors.size} ${onlyBoryaColors.joinToString(" ")}")
+}
 
 //задание через map
 fun main() {
